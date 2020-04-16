00fdbf1d08f8b39e5b60f188020364a32b97b090
Paweł Paterak <pawelpaterak@onet.pl>
2020-04-16 17:04:16 +0200

    Add Megiro Class Add KeyboardController class Add namespace Megiro2D.Controllers

---
+++ Megiro2D/Controllers/KeyboardController.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK;
+using OpenTK.Input;
+
+namespace Megiro2D.Controllers
+{
+    public class KeyboardController
+    {
+        public bool Alt { get; private set; }
+        public bool Command { get; private set; }
+        public bool Control { get; private set; }
+        public bool Shift { get; private set; }
+        public bool IsRepeat { get; private set; }
+
+        public event Key KeyDown;
+        public event Key KeyPress;
+        public event Key KeyUp;
+        public delegate void Key(char key);
+
+        public void OnKeyDown(object o, KeyboardKeyEventArgs e)
+        {
+            Alt = e.Alt;
+            Command = e.Command;
+            Control = e.Control;
+            Shift = e.Shift;
+            IsRepeat = e.IsRepeat;
+            KeyDown?.Invoke(e.Key.ToString()[0]);
+        }
+
+        public void OnKeyPress(object o, KeyPressEventArgs e)
+            => KeyPress?.Invoke(e.KeyChar);
+
+        public void OnKeyUp(object o, KeyboardKeyEventArgs e)
+        {
+            Alt = e.Alt;
+            Command = e.Command;
+            Control = e.Control;
+            Shift = e.Shift;
+            IsRepeat = e.IsRepeat;
+            KeyUp?.Invoke(e.Key.ToString()[0]);
+        }
+    }
+}
---
+++ Megiro2D/Megiro.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using Megiro2D.Controllers;
+
+namespace Megiro2D
+{
+    public class Megiro
+    {
+        public KeyboardController KeyboardController { get; set; } = new KeyboardController();
+
+        private readonly GameWindow window;
+
+        public Megiro(int width, int height, string title)
+        {
+            window = new GameWindow(width, height, GraphicsMode.Default, title);
+        }
+
+        public void Start()
+        {
+            window.KeyDown += KeyboardController.OnKeyDown;
+            window.KeyPress += KeyboardController.OnKeyPress;
+            window.KeyUp += KeyboardController.OnKeyUp;
+            window.Run(1.0/60.0);
+        }
+    }
+}
